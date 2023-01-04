2867a0c0b66fec79357f4276aca575712fc7711d
Ahecha <90638871+Ahecha@users.noreply.github.com>
2023-01-04 17:30:34 -0300

    base de js

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ function counter(question) {
 };
 
 question.addEventListener("click", counter);
+
+
+/* const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const button = question.querySelector(".question__button");
+  const questionText = question.querySelector(".question__title__text");
+  button.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if(item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+
+  questionText.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if(item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+}); */
